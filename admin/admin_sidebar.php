67e6c52dece831887ab6dd38f2b30ee5eb57f0ac
jaiminraval512 <jaiminraval512@gmail.com>
2026-01-20 21:47:26 +0530

    new changes

---
+++ admin/admin_sidebar.php
@@ -0,0 +1,24 @@
+<aside class="ad_side">
+    <div class="ad_logo">
+       <img src="/php/php_/hostel-management-system/assets/images/white_logo.png" alt="Logo">
+    </div>
+    <ul>
+        <li><a href="#"><i class="fas fa-user-graduate"></i> Students</a></li>
+        <li><a href="#"><i class="fas fa-door-open"></i> Rooms</a></li>
+        <li><a href="#"><i class="fas fa-money-check-alt"></i> Fees</a></li>
+        <li><a href="#"><i class="fas fa-calendar-check"></i> Attendance</a></li>
+        <li><a href="#"><i class="fas fa-exclamation-circle"></i> Complaints</a></li>
+       <li>
+    <a href="#">
+        <i class="fas fa-utensils"></i> 
+        Mess Menu
+    </a>
+</li>
+
+<li>
+    <a href="#" >
+        <i class="fas fa-file-signature"></i>Leave-Reports</a>
+</li>
+</li>
+    </ul>
+</aside>
